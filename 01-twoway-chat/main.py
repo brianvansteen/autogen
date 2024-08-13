4b040e3682ade7b99322162ec676264fd59a408a
brianvansteen <bvansteen@gmail.com>
2024-08-13 07:44:02 -0400

    basic agents with comments

--- 01-twoway-chat/main.py
+++ 01-twoway-chat/main.py
@@ -2,13 +2,13 @@ import autogen
 
 def main():
     config_list = autogen.config_list_from_json(
-        env_or_file="OAI_CONFIG_LIST.json"
+        env_or_file="OAI_CONFIG_LIST.json" # added to .gitignore
     )
 
     assistant = autogen.AssistantAgent(
         name="Assistant",
         llm_config={
-            "config_list": config_list
+            "config_list": config_list # get LLM and API key
         }
     )
 
@@ -16,7 +16,7 @@ def main():
         name="user",
         human_input_mode="NEVER",
         code_execution_config={
-            "work_dir": "coding",
+            "work_dir": "coding", # to store any output code
             "use_docker": False
         }
     )
